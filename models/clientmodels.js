5451fdff7961743e2befd3203a9160fe00a8ff06
Marwin NDONG ABOUROU <ndongabouroupavymarwin@gmail.com>
2025-04-04 10:45:54 +0200

    ajout du timestamps

--- models/clientmodels.js
+++ models/clientmodels.js
@@ -4,11 +4,12 @@ const clientSchema = new mongoose.Schema({
     nom: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     telephone: { type: String, required: true },
-    typeAssurance: { type: String, enum: ['auto', 'sante', 'risques divers'], required: '' },
-    dateExpiration: { type: Date, required: true }, 
-    impayes: { type: Boolean, default: true }, 
+    typeAssurance: { type: String, enum: ['auto', 'sante', 'risques divers'], required: true },
+    dateExpiration: { type: Date, required: true },
+    impayes: { type: Boolean, default: true },
     renouvellement: { type: Boolean, default: false },
-});
+}, 
+{ timestamps: true });
 
 const Client = mongoose.model('Client', clientSchema);
 
