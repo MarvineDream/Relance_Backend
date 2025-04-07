883a701244699f6bbc3913928fc9cd4867334bf1
Marwin NDONG ABOUROU <ndongabouroupavymarwin@gmail.com>
2025-04-07 09:27:06 +0200

    modification de toutes les routes en retirant les autorisations d'acces

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -1,24 +1,24 @@
 import express from 'express';
 import { addClient, deleteClient, getClient, getClientById, updateClient } from '../controllers/clientControllers.js';
-import { authenticate, authorize } from '../Middleware/auth.js';
+//import { authenticate, authorize } from '../Middleware/auth.js';
 
 const router = express.Router();
 
 
 // Route pour les clients
-router.post('/', authenticate, authorize, addClient);
+router.post('/',  addClient);
 
 // Route pour recuperer les clients
-router.get('/', getClient);
+router.get('/',  getClient);
 
 // Route pour recuperer un client par son id
-router.get('/:id', authenticate, authorize, getClientById);
+router.get('/:id',  getClientById);
 
 // Route pour mettre à jour un client
-router.put('/:id', authenticate, authorize, updateClient);
+router.put('/:id',  updateClient);
 
 // Route pour supprimer un client
-router.delete('/:id', authenticate, authorize, deleteClient);
+router.delete('/:id',  deleteClient);
 
 
 
